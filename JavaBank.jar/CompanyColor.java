fb812de53076a59563e298a1dc79ea39c8482e39
0607147683 <0607147683@2221-176-03.browardschools.local>
2019-08-28 11:16:41 -0400

    Initial code.

---
+++ JavaBank.jar/CompanyColor.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0607147683
+ */
+public final class CompanyColor {
+    private final int R = 238;
+    private final int G = 130;
+    private final int B = 238;
+    //this class uses the default constructor provided by java
+    
+    public int getR(){
+        return R;
+    }//end method getR
+    public int getG(){
+        return G;
+    }//end method getG
+    public int getB(){
+        return B;
+    }//end method getB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//end class CompanyColor
